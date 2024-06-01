4f022437efe757e2f2dc937b177d10dd79aa46c6
Manujdixit <94791934+Manujdixit@users.noreply.github.com>
2024-06-01 18:55:42 +0530

    Time: 142 ms (5.05%) | Memory: 52.6 MB (28.77%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -2,13 +2,24 @@
  * @param {number[]} nums
  * @return {number}
  */
+// var removeDuplicates = function (nums) {
+//     let index = 1
+//     for (let i = 0; i < nums.length - 1; i++) {
+//         if (nums[i] !== nums[i + 1]) {
+//             nums[index] = nums[i + 1]
+//             index++
+//         }
+//     }
+//     return index
+// }
+
 var removeDuplicates = function (nums) {
-    let index = 1
-    for (let i = 0; i < nums.length - 1; i++) {
-        if (nums[i] !== nums[i + 1]) {
-            nums[index] = nums[i + 1]
-            index++
+for(let i=0;i<nums.length;i++){
+    for(j=nums.length;j>=i+1;j--){
+        if (nums[i]==nums[j]){
+            nums.splice(i,1)
         }
+
     }
-    return index
+}
 }
