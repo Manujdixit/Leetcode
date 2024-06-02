7896014b6915213f0a442ea71bf45c88632a640a
Manujdixit <94791934+Manujdixit@users.noreply.github.com>
2024-06-02 15:01:43 +0530

    Time: 46 ms (97.99%) | Memory: 51.6 MB (18.67%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -3,11 +3,22 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    for(let i=0;i<nums.length;i++){
-        for(let j=i+1;j<nums.length;j++){
-            if(nums[i]+nums[j]==target){return [i,j]}
-        }
+// var twoSum = function(nums, target) {
+//     for(let i=0;i<nums.length;i++){
+//         for(let j=i+1;j<nums.length;j++){
+//             if(nums[i]+nums[j]==target){return [i,j]}
+//         }
+//     }
+//     return []
+// };
+
+var twoSum = function (nums, target) {
+    let map = new Map()
+    for (let i=0;i<nums.length;i++) {
+        let num1=nums[i]
+        let num2=target-num1
+        if(map.has(num2))return [i,map.get(num2)]
+        map.set(num1,i)
     }
-    return []
-};
+    
+}
