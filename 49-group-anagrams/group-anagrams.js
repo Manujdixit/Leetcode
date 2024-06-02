1c6b3d7b3045531571eb9d9fd4b6b25b6d807cbb
Manujdixit <94791934+Manujdixit@users.noreply.github.com>
2024-06-02 23:56:27 +0530

    Time: 96 ms (96.04%) | Memory: 61.2 MB (96.32%) - LeetSync

--- 49-group-anagrams/group-anagrams.js
+++ 49-group-anagrams/group-anagrams.js
@@ -2,14 +2,27 @@
  * @param {string[]} strs
  * @return {string[][]}
  */
-var groupAnagrams = function(strs) {
-    const map={}
+// var groupAnagrams = function(strs) {
+//     const map={}
 
-    for(const i of strs){
-        const key=i.split("").sort().join("")
-        if(map[key]){
-            map[key].push(i);
-        }else map[key]=[i]
+//     for(const i of strs){
+//         const key=i.split("").sort().join("")
+//         if(map[key]){
+//             map[key].push(i);
+//         }else map[key]=[i]
+//     }
+//     return Object.values(map);
+// };
+
+
+var groupAnagrams = function (strs) {
+    const map = new Map()
+
+    for (const i of strs) {
+        const key = i.split("").sort().join("")
+        if (map.has(key)) {
+            map.get(key).push(i)
+        } else map.set(key, [i]);
     }
-    return Object.values(map);
-};
+    return Array.from(map.values());
+}
