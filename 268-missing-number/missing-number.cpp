2f64c347f287e4fc9e390cdbe80fca764a007fbc
Manujdixit <manujdixit04@gmail.com>
2024-07-31 23:49:36 +0530

    Time: 11 ms (81.36%) | Memory: 20.7 MB (43.12%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -13,16 +13,34 @@
 //     }
 // };
 
+
+// class Solution {
+// public:
+//     int missingNumber(vector<int>& nums) {
+//         int n=nums.size();
+//         int sum=n*(n+1)/2;
+//         int sum2=0;
+//         for(int i=0;i<n;i++){
+//             sum2+=nums[i];
+//         }
+//         int missing=sum-sum2;
+//         return missing;
+//     }
+// };
+
+
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n=nums.size();
-        int sum=n*(n+1)/2;
-        int sum2=0;
+        int xor1=0;int xor2=0;
         for(int i=0;i<n;i++){
-            sum2+=nums[i];
+            xor1^=nums[i];
         }
-        int missing=sum-sum2;
+        for(int i=0;i<=n;i++){
+            xor2^=i;
+        }
+        int missing=xor1^xor2;
         return missing;
     }
 };
